add4e30e990e171046570866baf15d63e16fc73f
VLN37 <54167882+VLN37@users.noreply.github.com>
2021-10-15 03:29:43 -0300

    feat: get_slice tested and ready

--- ps_sorting4.c
+++ ps_sorting4.c
@@ -6,11 +6,29 @@
 /*   By: jofelipe <jofelipe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/15 00:33:22 by jofelipe          #+#    #+#             */
-/*   Updated: 2021/10/15 02:41:41 by jofelipe         ###   ########.fr       */
+/*   Updated: 2021/10/15 03:29:04 by jofelipe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	is_swap_valid(t_data data, int index)
+{
+	if (data.stack1[0] > data.stack1[1] && data.stack2[0] < data.stack2[1] && data.stack2[0] != data.stackmin && data.stack1[0] < data.sorted[index])
+		data = game("ss\n", data, &data.trialiter);
+	return (1);
+}
+
+int get_slice_size(t_data data)
+{
+	int	size;
+	if ((data.currslice + 1) * SLICE > data.nbr_count)
+		size = data.nbr_count % SLICE;
+	else
+		size = SLICE;
+	data.direction = get_direction(data);
+	return (size);
+}
 
 t_data	get_slice(t_data data)
 {
@@ -18,13 +36,14 @@ t_data	get_slice(t_data data)
 	int	index;
 
 	index = SLICE * data.currslice;
-	if ((data.currslice + 1) * SLICE > data.nbr_count)
-		numbers_left = data.nbr_count % SLICE;
-	else
-		numbers_left = SLICE;
+	numbers_left = get_slice_size(data);
 	data.direction = get_direction(data);
 	while (numbers_left)
 	{
+		if (data.stack1[0] > data.stack1[1] && data.stack2[0] < data.stack2[1] \
+		&& data.stack2[0] != data.stackmin && data.stack1[0] < data.sorted \
+		[index] && data.stack2[1] != STOP)
+		data = game("ss\n", data, &data.trialiter);
 		if (data.stack1[0] >= data.sorted[index])
 		{
 			data = game("pb\n", data, &data.trialiter);
@@ -32,11 +51,11 @@ t_data	get_slice(t_data data)
 			--numbers_left;
 		}
 		else if (data.direction == LEFT)
-			data = game("rra\n", data, &data.trialiter);
+			data = game("rrr\n", data, &data.trialiter);
 		else
-			data = game("ra\n", data, &data.trialiter);
+			data = game("rr\n", data, &data.trialiter);
 	}
-	data.currslice--;
+	--data.currslice;
 	return (data);
 }
 
